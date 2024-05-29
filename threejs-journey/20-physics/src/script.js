1c5ddbffa7406245ee4c04fe582d47105c0c6e3b
Reliable <olya.dyakiv26@gmail.com>
2024-05-29 16:53:10 +0200

    add threejs-journey/20-physics  (finish)

--- threejs-journey/20-physics/src/script.js
+++ threejs-journey/20-physics/src/script.js
@@ -1,7 +1,7 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import GUI from 'lil-gui'
-import CANNON from 'cannon'
+import * as CANNON from 'cannon-es'
 
 /**
  * Debug
@@ -11,8 +11,21 @@ const debugObject = {}
 debugObject.createSphere = () => {
     createSphere(Math.random() * 0.5, {x: (Math.random() - 0.5) * 3, y: 3, z: (Math.random() - 0.5) * 3})
 }
+debugObject.createBox = () => {
+    createBox(Math.random(), Math.random(), Math.random(), {x: (Math.random() - 0.5) * 3, y: 3, z: (Math.random() - 0.5) * 3})
+}
+
+debugObject.reset = () => {
+    for(const object of objNeedUpdate) {
+        object.body.removeEventListener('collide', playHitSound)
+        world.removeBody(object.body)
+        scene.remove(object.mesh)
+    }
+}
 
 gui.add(debugObject, 'createSphere')
+gui.add(debugObject, 'createBox')
+gui.add(debugObject, 'reset')
 
 /**
  * Base
@@ -22,6 +35,19 @@ const canvas = document.querySelector('canvas.webgl')
 
 // Scene
 const scene = new THREE.Scene()
+
+// Sound
+
+const hitSound = new Audio('./sounds/hit.mp3')
+
+const playHitSound = (collision) => {
+    const impactStenght = collision.contact.getImpactVelocityAlongNormal()
+    if(impactStenght > 1.5) {
+        hitSound.volume = Math.random()
+        hitSound.currentTime = 0
+        hitSound.play()
+    } 
+}
 
 /**
  * Textures
@@ -42,6 +68,8 @@ const environmentMapTexture = cubeTextureLoader.load([
 
 // World
 const world = new CANNON.World()
+world.broadphase = new CANNON.SAPBroadphase(world)
+world.allowSleep = true
 world.gravity.set(0, -9.82, 0)
 
 // Materials
@@ -178,6 +206,7 @@ const createSphere = (radius,position) => {
         material: defaultMaterial
     })
     body.position.copy(position)
+    body.addEventListener('collide', playHitSound)
     world.addBody(body)
     objNeedUpdate.push({
         mesh,
@@ -185,7 +214,45 @@ const createSphere = (radius,position) => {
     })
 }
 
+
 createSphere(0.5, {x: 0, y: 3, z: 0})
+
+// Box
+
+const boxGeometry = new THREE.BoxGeometry(1,1,1)
+const boxMaterial = new THREE.MeshStandardMaterial({
+     metalness: 0.3,
+     roughness: 0.4,
+     envMap: environmentMapTexture
+ })
+
+const createBox = (width,height,depth,position) => {
+    // THREE JS MESH
+    const mesh = new THREE.Mesh(
+        boxGeometry,
+        boxMaterial
+    )
+    mesh.scale.set(width, height, depth)
+    mesh.castShadow = true
+    mesh.position.copy(position)
+    scene.add(mesh)
+    
+    // CANNON JS 
+    const shape = new CANNON.Box(new CANNON.Vec3(width / 2, height / 2, depth / 2))
+    const body = new CANNON.Body({
+        mass: 1,
+        position: new CANNON.Vec3(0,0,0),
+        shape,
+        material: defaultMaterial
+    })
+    body.position.copy(position)
+    body.addEventListener('collide', playHitSound)
+    world.addBody(body)
+    objNeedUpdate.push({
+        mesh,
+        body
+    })
+}
 
 /**
  * Animate
@@ -201,6 +268,7 @@ const tick = () =>
 
     for(const object of objNeedUpdate) {
         object.mesh.position.copy(object.body.position)
+        object.mesh.quaternion.copy(object.body.quaternion)
     }
 
 
