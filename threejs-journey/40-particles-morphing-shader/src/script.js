4d3a8b3704bf8841b935d15bd225e310a4999fce
Reliable <olya.dyakiv26@gmail.com>
2024-06-26 13:25:15 +0200

    add threejs-journey/40-particles-morphing-shader (start)

---
+++ threejs-journey/40-particles-morphing-shader/src/script.js
@@ -0,0 +1,170 @@
+import * as THREE from 'three'
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js'
+import { DRACOLoader } from 'three/addons/loaders/DRACOLoader.js'
+import GUI from 'lil-gui'
+import gsap from 'gsap'
+import particlesVertexShader from './shaders/particles/vertex.glsl'
+import particlesFragmentShader from './shaders/particles/fragment.glsl'
+
+/**
+ * Base
+ */
+// Debug
+const gui = new GUI({ width: 340 })
+const debugObject = {}
+
+// Canvas
+const canvas = document.querySelector('canvas.webgl')
+
+// Scene
+const scene = new THREE.Scene()
+
+// Loaders
+const dracoLoader = new DRACOLoader()
+dracoLoader.setDecoderPath('./draco/')
+const gltfLoader = new GLTFLoader()
+gltfLoader.setDRACOLoader(dracoLoader)
+
+/**
+ * Sizes
+ */
+const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight,
+    pixelRatio: Math.min(window.devicePixelRatio, 2)
+}
+
+window.addEventListener('resize', () =>
+{
+    // Update sizes
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
+    sizes.pixelRatio = Math.min(window.devicePixelRatio, 2)
+
+    // Materials
+    if(particles) 
+        particles.material.uniforms.uResolution.value.set(sizes.width * sizes.pixelRatio, sizes.height * sizes.pixelRatio)
+
+    // Update camera
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
+
+    // Update renderer
+    renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(sizes.pixelRatio)
+})
+
+/**
+ * Camera
+ */
+// Base camera
+const camera = new THREE.PerspectiveCamera(35, sizes.width / sizes.height, 0.1, 100)
+camera.position.set(0, 0, 8 * 2)
+scene.add(camera)
+
+// Controls
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas,
+    antialias: true,
+})
+
+renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(sizes.pixelRatio)
+
+debugObject.clearColor = '#160920'
+gui.addColor(debugObject, 'clearColor').onChange(() => { renderer.setClearColor(debugObject.clearColor) })
+renderer.setClearColor(debugObject.clearColor)
+
+/**
+ * Particles
+ */
+
+// Load model
+let particles = null
+
+gltfLoader.load('./models.glb', (gltf) => {
+
+    particles = {}
+
+    // Positions
+    const positions = gltf.scene.children.map(child => child.geometry.attributes.position)
+    particles.maxCount = 0
+    for(const position of positions) {
+        if(position.count > particles.maxCount)
+        particles.maxCount = position.count
+    }
+
+    particles.positions = []
+
+    for(const position of positions) {
+        const originalArray = position.array
+        const newArray = new Float32Array(particles.maxCount * 3)
+
+        for(let i = 0; i < particles.maxCount; i++) {
+            const i3 = i * 3
+            if(i3 < originalArray.length) {
+                newArray[i3 + 0] = originalArray[i3 + 0]
+                newArray[i3 + 1] = originalArray[i3 + 1]
+                newArray[i3 + 2] = originalArray[i3 + 2]
+            } else {
+                const randomIndex = Math.floor(position.count * Math.random()) * 3
+                newArray[i3 + 0] = originalArray[randomIndex + 0]
+                newArray[i3 + 1] = originalArray[randomIndex + 1]
+                newArray[i3 + 2] = originalArray[randomIndex + 2]
+            }
+        }
+
+        particles.positions.push(new THREE.Float32BufferAttribute(newArray, 3))
+    }
+
+    // Geometry
+    particles.geometry = new THREE.BufferGeometry()
+    particles.geometry.setAttribute('position', particles.positions[1])
+    particles.geometry.setAttribute('aPositionTarget', particles.positions[3])
+
+
+    // Material
+    particles.material = new THREE.ShaderMaterial({
+        vertexShader: particlesVertexShader,
+        fragmentShader: particlesFragmentShader,
+        blending: THREE.AdditiveBlending,
+        depthWrite: false,
+        uniforms:
+        {
+            uSize: new THREE.Uniform(0.2),
+            uResolution: new THREE.Uniform(new THREE.Vector2(sizes.width * sizes.pixelRatio, sizes.height * sizes.pixelRatio)),
+            uProgress: new THREE.Uniform(0)
+        }
+    })
+
+    // Points
+    particles.points = new THREE.Points(particles.geometry, particles.material)
+    scene.add(particles.points)
+
+    // Tweaks
+    gui.add(particles.material.uniforms.uProgress, 'value').min(0).max(1).step(0.001).name('uProgress')
+})
+
+/**
+ * Animate
+ */
+const tick = () =>
+{
+    // Update controls
+    controls.update()
+
+    // Render normal scene
+    renderer.render(scene, camera)
+
+    // Call tick again on the next frame
+    window.requestAnimationFrame(tick)
+}
+
+tick()
