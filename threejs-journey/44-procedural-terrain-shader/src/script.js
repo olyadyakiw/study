2849be561b82418db3cb41bbe97445fea8fdc341
Reliable <olya.dyakiv26@gmail.com>
2024-07-01 11:56:46 +0200

    add threejs-journey/44-procedural-terrain-shader (start)

---
+++ threejs-journey/44-procedural-terrain-shader/src/script.js
@@ -0,0 +1,187 @@
+import * as THREE from 'three'
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
+import { RGBELoader } from 'three/addons/loaders/RGBELoader.js'
+import { SUBTRACTION, Evaluator, Brush } from 'three-bvh-csg'
+import CustomShaderMaterial from 'three-custom-shader-material/vanilla'
+import terrainVertexShader from './shaders/terrain/vertex.glsl'
+import terrainFragmentShader from './shaders/terrain/fragment.glsl'
+import GUI from 'lil-gui'
+
+/**
+ * Base
+ */
+// Debug
+const gui = new GUI({ width: 325 })
+const debugObject = {}
+
+// Canvas
+const canvas = document.querySelector('canvas.webgl')
+
+// Scene
+const scene = new THREE.Scene()
+
+// Loaders
+const rgbeLoader = new RGBELoader()
+
+/**
+ * Environment map
+ */
+rgbeLoader.load('/spruit_sunrise.hdr', (environmentMap) =>
+{
+    environmentMap.mapping = THREE.EquirectangularReflectionMapping
+
+    scene.background = environmentMap
+    scene.backgroundBlurriness = 0.5
+    scene.environment = environmentMap
+})
+
+/**
+ * Terrain
+ */
+// geometry
+const geometry = new THREE.PlaneGeometry(10,10,500,500)
+geometry.deleteAttribute('uv')
+geometry.deleteAttribute('normal')
+geometry.rotateX(- Math.PI * 0.5)
+
+const uniforms = {
+    uPositionFrequence: new THREE.Uniform(0.2),
+    uStrength: new THREE.Uniform(2.0), 
+    uWrapFrequence: new THREE.Uniform(5),
+    uWarpStrength: new THREE.Uniform(0.5),
+}
+
+gui.add(uniforms.uPositionFrequence, 'value', 0, 1, 0.001).name('uPositionFrequence')
+gui.add(uniforms.uStrength, 'value', 0, 10, 0.001).name('uStrength')
+gui.add(uniforms.uWrapFrequence, 'value', 0, 10, 0.001).name('uWrapFrequence')
+gui.add(uniforms.uWarpStrength, 'value', 0, 1, 0.001).name('uWarpStrength')
+
+// material
+const material = new CustomShaderMaterial({
+    // CSM
+    baseMaterial: THREE.MeshStandardMaterial,
+    vertexShader: terrainVertexShader,
+    fragmentShader: terrainFragmentShader,
+    uniforms: uniforms,
+    silent: true,
+
+    // MeshStandardMaterial
+    metalness: 0,
+    roughness: 0.5,
+    color: '#85d534'
+})
+
+// mesh
+const terrain = new THREE.Mesh(geometry, material)
+scene.add(terrain)
+terrain.receiveShadow = true
+terrain.castShadow = true
+
+/**
+ * Board
+ */
+const boardFill = new Brush(new THREE.BoxGeometry(11, 2, 11))
+const boardHole = new Brush(new THREE.BoxGeometry(10, 2.1, 10))
+boardFill.material.color.set('red')
+boardHole.material = new THREE.MeshNormalMaterial()
+
+// Evaluate
+const evaluator = new Evaluator()
+
+const board = evaluator.evaluate(boardFill, boardHole, SUBTRACTION)
+board.geometry.clearGroups()
+board.material = new THREE.MeshStandardMaterial({
+    color: '#ffffff',
+    metalness: 0,
+    roughness: 0.3
+})
+board.castShadow = true
+board.receiveShadow = true
+scene.add(board)
+
+/**
+ * Lights
+ */
+const directionalLight = new THREE.DirectionalLight('#ffffff', 2)
+directionalLight.position.set(6.25, 3, 4)
+directionalLight.castShadow = true
+directionalLight.shadow.mapSize.set(1024, 1024)
+directionalLight.shadow.camera.near = 0.1
+directionalLight.shadow.camera.far = 30
+directionalLight.shadow.camera.top = 8
+directionalLight.shadow.camera.right = 8
+directionalLight.shadow.camera.bottom = -8
+directionalLight.shadow.camera.left = -8
+scene.add(directionalLight)
+
+/**
+ * Sizes
+ */
+const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight,
+    pixelRatio: Math.min(window.devicePixelRatio, 2)
+}
+
+window.addEventListener('resize', () =>
+{
+    // Update sizes
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
+    sizes.pixelRatio = Math.min(window.devicePixelRatio, 2)
+
+    // Update camera
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
+
+    // Update renderer
+    renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(sizes.pixelRatio)
+})
+
+/**
+ * Camera
+ */
+// Base camera
+const camera = new THREE.PerspectiveCamera(35, sizes.width / sizes.height, 0.1, 100)
+camera.position.set(-10, 6, -2)
+scene.add(camera)
+
+// Controls
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas,
+    antialias: true
+})
+renderer.shadowMap.enabled = true
+renderer.shadowMap.type = THREE.PCFSoftShadowMap
+renderer.toneMapping = THREE.ACESFilmicToneMapping
+renderer.toneMappingExposure = 1
+renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(sizes.pixelRatio)
+
+/**
+ * Animate
+ */
+const clock = new THREE.Clock()
+
+const tick = () =>
+{
+    const elapsedTime = clock.getElapsedTime()
+
+    // Update controls
+    controls.update()
+
+    // Render
+    renderer.render(scene, camera)
+
+    // Call tick again on the next frame
+    window.requestAnimationFrame(tick)
+}
+
+tick()
---
+++ threejs-journey/44-procedural-terrain-shader/src/shaders/terrain/vertex.glsl
@@ -0,0 +1,41 @@
+#include ../includes/simplexNoise2d.glsl
+
+uniform float uPositionFrequence;
+uniform float uStrength;
+uniform float uWrapFrequence;
+uniform float uWarpStrength;
+
+float getElevation(vec2 position) {
+
+    vec2 warpedPosition = position;
+    warpedPosition += simplexNoise2d(warpedPosition * uWrapFrequence * uPositionFrequence) * uWarpStrength;
+
+    float elevation = 0.0;
+    elevation += simplexNoise2d(warpedPosition * uPositionFrequence) / 2.0;
+    elevation += simplexNoise2d(warpedPosition * uPositionFrequence * 2.0) / 4.0;
+    elevation += simplexNoise2d(warpedPosition * uPositionFrequence * 4.0) / 8.0;
+
+    float elevationSign = sign(elevation);
+    elevation = pow(abs(elevation), 2.0) * elevationSign;
+    elevation *= uStrength;
+
+    return elevation;
+}
+
+void main() {
+    // Neighbours position
+    float shift = 0.01;
+    vec3 positionA = position + vec3(shift, 0.0, 0.0);
+    vec3 positionB = position + vec3(0.0, 0.0, -shift);
+
+    // Elevation
+    float elevation = getElevation(csm_Position.xz);
+    positionA.y = getElevation(positionA.xz);
+    positionB.y = getElevation(positionA.xz);
+    csm_Position.y += elevation;
+
+    // Compute normal
+    vec3 toA = normalize(positionA - csm_Position);
+    vec3 toB = normalize(positionB - csm_Position);
+    csm_Normal = cross(toA, toB);
+}
