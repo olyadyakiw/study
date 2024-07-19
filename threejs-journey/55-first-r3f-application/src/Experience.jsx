7da9e5d76464294d3bf0fded26b4bffe299055a5
Olha Diakiv <olya.dyakiv26@gmail.com>
2024-07-19 17:53:09 +0200

    add threejs-journey/55-first-r3f-application (finish)

--- threejs-journey/55-first-r3f-application/src/Experience.jsx
+++ threejs-journey/55-first-r3f-application/src/Experience.jsx
@@ -2,6 +2,8 @@ import { useThree, extend, useFrame } from "@react-three/fiber"
 import { useRef } from "react"
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 
+import CustomObject from "./CustomObject.jsx"
+
 extend({ OrbitControls })
 
 export default function Experience() {
@@ -9,26 +11,37 @@ export default function Experience() {
 
     const cubeRef = useRef()
     const groupRef = useRef()
+
     useFrame((state, delta) => {
         cubeRef.current.rotation.y += delta
         // groupRef.current.rotation.y += delta
+
+        // const angle = state.clock.elapsedTime
+        // state.camera.position.x = Math.sin(angle) * 8
+        // state.camera.position.z = Math.cos(angle) * 8
+        // state.camera.lookAt(0, 0, 0)
     })
     return <>
         <orbitControls args={ [ camera, gl.domElement ] } />
 
+        <directionalLight position={ [ 1, 2, 3 ] } intensity={ 1.5 } />
+        <ambientLight intensity={ 0.5 } />
+
         <group ref={ groupRef }>
             <mesh ref={ cubeRef } scale={ 1.5 } position-x={ 2 } rotation-y={ Math.PI * 0.25 } >
                 <boxGeometry scale={ 1.5 } />
-                <meshBasicMaterial color="mediumpurple" />
+                <meshStandardMaterial color="mediumpurple" />
             </mesh>
             <mesh position-x={ -2 } rotation-y={ Math.PI * 0.25 } >
                 <sphereGeometry />
-                <meshBasicMaterial color="orange" />
+                <meshStandardMaterial color="orange" />
             </mesh>
         </group>
         <mesh position-y={ -1 } rotation-x={ - Math.PI * 0.5 } scale={ 10 }>
             <planeGeometry />
-            <meshBasicMaterial color="greenyellow" />
+            <meshStandardMaterial color="greenyellow" />
         </mesh>
+
+        <CustomObject />
     </>
 }
--- threejs-journey/55-first-r3f-application/src/index.jsx
+++ threejs-journey/55-first-r3f-application/src/index.jsx
@@ -2,11 +2,18 @@ import './style.css'
 import ReactDOM from 'react-dom/client'
 import { Canvas } from '@react-three/fiber'
 import Experience from './Experience'
+import * as THREE from 'three'
 
 const root = ReactDOM.createRoot(document.querySelector('#root'))
 
 root.render(
-    <Canvas>
+    <Canvas
+    gl={ {
+            antialias: true,
+            toneMapping: THREE.ACESFilmicToneMapping
+        } }
+        camera={ { fov: 45, near: 0.1, far: 200, position: [ 3, 2, 6 ] } }
+    >
         <Experience />
     </Canvas>
 )
