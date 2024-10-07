2a604fb7ebb33e442130808c955d643be14f4fba
Olha Diakiv <olya.dyakiv26@gmail.com>
2024-10-07 16:56:15 +0200

    js-course: start 01-Fundamentals

---
+++ complete-javascript-course/01-Fundamentals-Part-1/assignments.js
@@ -0,0 +1,5 @@
+let country = 'Ukraine'
+let continent = 'Europe'
+let population = 40
+
+console.log(country, continent, population)
---
+++ complete-javascript-course/01-Fundamentals-Part-1/script.js
@@ -0,0 +1,17 @@
+let js = 'amazing'
+console.log('Olya')
+console.log(40 + 8 - 10)
+
+let firstName = 'Olya'
+console.log(firstName)
+console.log(firstName)
+console.log(firstName)
+
+let $function = 27
+
+let PI = 3.1415
+
+let myFirstJob = 'coder'
+let myCurrentJob = 'teacher'
+
+console.log(myFirstJob)
