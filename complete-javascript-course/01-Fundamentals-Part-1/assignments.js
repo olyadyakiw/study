3d268b972c0470319b22ec02a6ca58a68d30ebfc
Olha Diakiv <olya.dyakiv26@gmail.com>
2024-10-08 11:15:52 +0200

    js-course: continue 01-Fundamentals

--- complete-javascript-course/01-Fundamentals-Part-1/assignments.js
+++ complete-javascript-course/01-Fundamentals-Part-1/assignments.js
@@ -1,5 +1,19 @@
-let country = 'Ukraine'
-let continent = 'Europe'
-let population = 40
+// let country = 'Ukraine'
+// const continent = 'Europe'
+// let population = 40
 
-console.log(country, continent, population)
+// console.log(country, continent, population)
+
+// const isIsland = false
+// let language
+
+// console.log(isIsland, population, country, language)
+
+// language = 'ukranian'
+
+// console.log(population / 2)
+// population++
+// console.log(6 > population)
+// console.log(33 > population)
+// const description = country + ' is in ' + continent + ', and its ' + population + ' million people speak ' + language
+// console.log(description)
--- complete-javascript-course/01-Fundamentals-Part-1/script.js
+++ complete-javascript-course/01-Fundamentals-Part-1/script.js
@@ -1,17 +1,92 @@
-let js = 'amazing'
-console.log('Olya')
-console.log(40 + 8 - 10)
+// let js = 'amazing'
+// console.log('Olya')
+// console.log(40 + 8 - 10)
 
-let firstName = 'Olya'
-console.log(firstName)
-console.log(firstName)
-console.log(firstName)
+// let firstName = 'Olya'
+// console.log(firstName)
+// console.log(firstName)
+// console.log(firstName)
 
-let $function = 27
+// let $function = 27
 
-let PI = 3.1415
+// let PI = 3.1415
 
-let myFirstJob = 'coder'
-let myCurrentJob = 'teacher'
+// let myFirstJob = 'coder'
+// let myCurrentJob = 'teacher'
 
-console.log(myFirstJob)
+// console.log(myFirstJob)
+
+// true
+// let javascriptIsFun = true
+// console.log(javascriptIsFun)
+
+// console.log(typeof true)
+// console.log(typeof javascriptIsFun)
+// console.log(typeof 23)
+// console.log(typeof 'gfdgdf')
+
+// javascriptIsFun = 'YES'
+// console.log(typeof javascriptIsFun)
+
+// let year
+// console.log(year)
+// console.log(typeof year)
+
+// year = 1998
+// console.log(typeof year)
+
+// console.log(typeof null)
+
+// let age = 30
+// age = 31
+
+// const birthday = 1998
+// // birthday = 1997
+
+// // const job
+
+// var job = 'programmer'
+// job = 'teacher'
+
+// const now = 2037
+// const ageOlya = now - 1998
+// const ageSarah = now - 2018
+// console.log(ageOlya)
+// console.log(ageSarah)
+
+// console.log(ageOlya * 2)
+// console.log(ageOlya / 10)
+// console.log(2 ** 3)
+
+// const firstName = 'Olya'
+// const lastName = 'Diakiv'
+// console.log(firstName + " " + lastName)
+
+// let x = 10 + 5
+// x += 10
+// x *= 4
+// x++
+// x--
+// x--
+// console.log(x)
+
+// console.log(ageOlya > ageSarah)
+// console.log(ageSarah >= 18)
+
+// const isFullAge = ageSarah >= 18
+
+// console.log(now - 1998 > now - 2018)
+
+const now = 2037
+const ageOlya = now - 1998
+const ageSarah = now - 2018
+console.log(now - 1998 > now - 2018)
+
+console.log(25 - 10 - 5)
+
+x = y = 25 - 10 -5
+console.log(x, y)
+
+console.log(ageOlya, ageSarah)
+const averageAge = (ageOlya + ageSarah) / 2
+console.log(averageAge)
