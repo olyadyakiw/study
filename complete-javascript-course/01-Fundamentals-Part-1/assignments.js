662663bef50d08141e8c7285556d5a2ed3c5b7c5
Olha Diakiv <olya.dyakiv26@gmail.com>
2024-10-09 12:59:35 +0200

    js-course: continue 01-Fundamentals

--- complete-javascript-course/01-Fundamentals-Part-1/assignments.js
+++ complete-javascript-course/01-Fundamentals-Part-1/assignments.js
@@ -17,3 +17,33 @@
 // console.log(33 > population)
 // const description = country + ' is in ' + continent + ', and its ' + population + ' million people speak ' + language
 // console.log(description)
+
+// const newDescription = `${country} is in ${continent}, and its ${population} million people speak ${language}`
+// console.log(newDescription)
+
+// if( population > 33) {
+//     console.log(`${country}'s population is ${population - 33} million above average`)
+// }
+
+// // console.log('9' - '5'); // -> 4
+// // console.log('19' - '13' + '17'); // -> 617
+// // console.log('19' - '13' + 17); // -> 23
+// // console.log('123' < 57); // -> false
+// // console.log(5 + 6 + '4' + 9 - 4 - 2); // -> 1143
+
+// // // const numNeighbours = +prompt('How many neighbour countries does your contry have?')
+
+// // if(numNeighbours === 1) {
+// //     console.log('Only 1 border!')
+// // } else if(numNeighbours > 1) {
+// //     console.log('More than 1 border')
+// // } else {
+// //     console.log('No borders')
+// // }
+
+
+// if( language === 'english' && population < 50 && !isIsland ) {
+//     console.log(`You should live in ${country}`)
+// } else {
+//     console.log(`${country} does not meet your criteria :(`)
+// }
--- complete-javascript-course/01-Fundamentals-Part-1/script.js
+++ complete-javascript-course/01-Fundamentals-Part-1/script.js
@@ -77,16 +77,119 @@
 
 // console.log(now - 1998 > now - 2018)
 
-const now = 2037
-const ageOlya = now - 1998
-const ageSarah = now - 2018
-console.log(now - 1998 > now - 2018)
+// const now = 2037
+// const ageOlya = now - 1998
+// const ageSarah = now - 2018
+// console.log(now - 1998 > now - 2018)
 
-console.log(25 - 10 - 5)
+// console.log(25 - 10 - 5)
 
-x = y = 25 - 10 -5
-console.log(x, y)
+// x = y = 25 - 10 -5
+// console.log(x, y)
 
-console.log(ageOlya, ageSarah)
-const averageAge = (ageOlya + ageSarah) / 2
-console.log(averageAge)
+// console.log(ageOlya, ageSarah)
+// const averageAge = (ageOlya + ageSarah) / 2
+// console.log(averageAge)
+
+// const firstName = 'Olya'
+// const job = 'programmer'
+// const birthYear = 1998
+// const year = 2037
+
+// const olya = `I'm ${firstName}, a ${year - birthYear} year old ${job}!`
+// console.log(olya)
+
+// console.log(`
+//     hello
+//     olya
+//     `)
+
+// const age = 15
+
+// if(age >= 18) {
+//     console.log('Sarah can start 😃')
+// } else {
+//     const yearsLeft = 18 - age
+//     console.log(`Sara is too young. Wait another ${yearsLeft} years`)
+// }
+
+// const birthYear = 1998
+// let century
+// if(birthYear <= 2000) {
+//     century = 20
+// } else {
+//     century = 21
+// }
+
+// console.log(century)
+
+// const inputYear = '1998'
+// console.log(Number(inputYear))
+// console.log(Number(inputYear) + 18)
+
+// console.log(Number('olya'))
+// console.log(typeof NaN)
+
+// console.log(String(1998))
+
+// console.log("I'am " + 23 + ' years old')
+
+// console.log('23' - '10' - 3)
+// console.log('23' * '2')
+
+// console.log(Boolean(0))
+// console.log(Boolean(undefined))
+// console.log(Boolean('Jonas'))
+// console.log(Boolean({}))
+// console.log(Boolean(NaN))
+
+// const money = 100
+
+// if(money) {
+//     console.log('Dont spend it all!')
+// } else {
+//     console.log('You should get a job')
+// }
+
+// let height
+
+// if(height) {
+//     console.log('Yay')
+// } else {
+//     console.log('no height')
+// }
+
+// const age = '18'
+// if(age === 18) console.log('Ypu just an adult (strict)')
+// if(age == 18) console.log('Ypu just an adult (loose)')
+
+// const favourite = +prompt('What is your facourite number?')
+// console.log(favourite)
+// console.log(typeof favourite)
+
+// if(favourite === 23) {
+//     console.log('cool 23')
+// } else if (favourite === 7) {
+//     console.log('7 is also cool')
+// } else {
+//     console.log(favourite)
+// }
+
+// if(favourite !== 22) {
+//     console.log('not 22')
+// }
+
+// const hasDriversLicence = true
+// const hasGoodVision = true
+// const isTired = false
+
+// console.log(hasDriversLicence && hasGoodVision)
+// console.log(hasDriversLicence || hasGoodVision)
+// console.log(!hasDriversLicence)
+
+
+// if(hasDriversLicence && hasGoodVision && !isTired) {
+//     console.log('SHould drive')
+// } else {
+//     console.log('❌')
+// }
