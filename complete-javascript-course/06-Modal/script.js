bc13128031b7609105ddbad65e9002b660061cf1
Olha Diakiv <olya.dyakiv26@gmail.com>
2024-10-30 14:04:21 +0100

    js-course: add 06-modal

--- complete-javascript-course/06-Modal/script.js
+++ complete-javascript-course/06-Modal/script.js
@@ -1,1 +1,26 @@
-'use strict';
+'use strict'
+
+const modal = document.querySelector('.modal')
+const overlay = document.querySelector('.overlay')
+const btnCloseBtn = document.querySelector('.close-modal')
+const btnsShowModal = document.querySelectorAll('.show-modal')
+
+const openModal = () => {
+    modal.classList.remove('hidden')
+    overlay.classList.remove('hidden')
+}
+
+const closeModal = () => {
+    modal.classList.add('hidden')
+    overlay.classList.add('hidden')
+}
+
+for (let i = 0; i < btnsShowModal.length; i++) btnsShowModal[i].addEventListener('click', openModal)
+
+btnCloseBtn.addEventListener('click', closeModal)
+
+overlay.addEventListener('click', closeModal)
+
+document.addEventListener('keydown', e => {
+    if (e.key == 'Escape' && !modal.classList.contains('hidden')) closeModal()
+})
