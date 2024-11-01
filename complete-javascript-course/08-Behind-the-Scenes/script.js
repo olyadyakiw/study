eb168bb85a1477c882af6b31a941498ff4493989
Olha Diakiv <olya.dyakiv26@gmail.com>
2024-11-01 16:54:53 +0100

    js-course: add 08-behind-the-scenes

--- complete-javascript-course/08-Behind-the-Scenes/script.js
+++ complete-javascript-course/08-Behind-the-Scenes/script.js
@@ -1,1 +1,28 @@
-'use strict';
+'use strict'
+
+function calcAge(birthYear) {
+    const age = 2037 - birthYear
+    console.log(firstName)
+
+    function printAge() {
+        let output = `${firstName}, you are ${age} years, born in ${birthYear}`
+        console.log(output)
+
+        if (birthYear >= 1981 && birthYear <= 1998) {
+            const firstName = 'OlhaF'
+            const string = `you're millenial, ${firstName}`
+            console.log(string)
+
+            output = 'New output'
+        }
+
+        console.log(output)
+    }
+
+    printAge()
+
+    return age
+}
+
+const firstName = 'Olha'
+calcAge(1998)
